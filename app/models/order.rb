26f5cb341f6e02cb469a49324c606d4ceaf5c84a
szewczykmira <szewczyk.mira@gmail.com>
2014-10-25 22:42:57 +0200

    Assertions

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
   validates :customer_name, :street, :city, presence: true
   validates :customer_name, uniqueness: true
+  has_many :order_lines
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
   validates :name, :price, presence: true
   validates :name, uniqueness: true
+
+  has_many :order_lines
 end
